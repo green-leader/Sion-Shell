2aebf090e86f0eb11404210c05338820f0a5a6f3
Sion F <fandrick.sion@gmail.com>
2019-04-20 11:10:35 -0600

    Testing wasn't commited

---
+++ test_basic.py
@@ -0,0 +1,22 @@
+import sshell
+import unittest
+
+
+class TestCommandMethods(unittest.TestCase):
+
+    def test_parseCommand(self):
+        self.assertEqual(sshell.parseCommand(''), [''])
+        cmdIn = 'ls -l /tmp'
+        self.assertEqual(sshell.parseCommand(cmdIn), ['ls', '-l', '/tmp'])
+
+    def test_builtin_exit(self):
+        with self.assertRaises(SystemExit) as exitCode:
+            sshell.runCommand(['exit'])
+        self.assertEqual(exitCode.exception.code, 0)
+        with self.assertRaises(SystemExit) as exitCode:
+            sshell.runCommand(['exit', '-1'])
+        self.assertEqual(exitCode.exception.code, -1)
+
+
+if __name__ == '__main__':
+    unittest.main()
